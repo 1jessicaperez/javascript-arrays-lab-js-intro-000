baf451fe473a2cc4df8e7637bd7ece0ec8c008fc
Jessica Perez <1jessicaperez@gmail.com>
2020-06-11 20:17:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push("Ralph")
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift("Bob")
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  var newArray= [...kittens, "Broom"]
+  return newArray
+}
